544a129ff264583319fa2f4d70f960b33715ba03
Fryed Biscuit <fryedbiscuit@gmail.com>
2026-05-21 23:33:25 -0300

    [advent] 2015/2 Solved second part

--- advent/2015/2/main.c
+++ advent/2015/2/main.c
@@ -1,12 +1,25 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
-long min(long a, long b, long c) {
+long max3(long a, long b, long c) {
+	long max = a;
+	if (b > max) max = b;
+	if (c > max) max = c;
+	return max;
+}
+
+long min3(long a, long b, long c) {
 	// return the smallest of the arguments
 	long min = a;
 	if (b < min) min = b;
 	if (c < min) min = c;
 	return min;
+}
+
+long get_ribbon_length(long l, long w, long h) {
+	long wrap_lenght = (l + w + h - max3(l,w,h)) * 2;
+	long bow_lenght = l * w * h;
+	return wrap_lenght + bow_lenght;
 }
 
 int main() {
@@ -18,13 +31,14 @@ int main() {
 	size_t llenght = 0;
 
 	long l,w,h;
-	long a,b,c;
-	long result = 0;
+	long total_area = 0;
+	long ribbon_length = 0;
 
 	while ((getline(&line_buffer,&llenght,input)) != -1) {
 		sscanf(line_buffer,"%ldx%ldx%ld", &l,&w,&h);
-		result += (2 * l * w) + (2 * w * h) + (2 * h * l) + min(l*w, w*h, l*h);
+		total_area += (2 * l * w) + (2 * w * h) + (2 * h * l) + min3(l*w, w*h, l*h);
+		ribbon_length += get_ribbon_length(l,w,h);
 	}
 
-	printf("They should order %ld feet.\n", result);
+	printf("They should order %ld feet. Plus %ld feet of ribbon.\n", total_area, ribbon_length);
 }
